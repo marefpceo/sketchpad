f48a11a532456bc50ae28c2b1d1c8977655cc4f0
marefpceo <marefpceo@gmail.com>
2022-10-28 09:32:40 -0700

    Updated to allow use of mouse button to draw
    Refactored code in main.js and removed functions that were identified as
    unnecessary.
    
    README.md was updated to reflect the new changes.

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # Sketchpad
 
 Sketchpad is an online browser version of the popular Etch-A-Sketch toy. The 
-page default is set to a 16x16 canvas area.  When the user drags the mouse pointer across the canvas area, the grid squares will change colors. 
+page default is set to a 16x16 canvas area. The user is able to draw on the canvas by pressing, and holding, the left mouse button while dragging across the screen.
+
+## Grid Size
+
+To change the grid size, select the 'Change Size' button on the left hand side. (Grid size ranges from 1 to 100) 
+
+## Color Options
+
+Color options can be selecting using the option buttons in the 'Color' area. 
 
 The initial setting is black on white. 
 
 The 'Random' setting randomly selects a color while moving the mouse pointer across the grid.
 
-Selecting 'Erase' will restore any colored squares back to white.  
+Selecting 'Erase' will return any colored squares back to white when drawing on the canvas.  
+
+The 'Select' setting will give the user access to the color palet below, allowing selection of any color. 
 
 To quickly erase the entire canvas, select the 'Clear' button at the bottom of the page.  
-
-To change the grid size, select the 'Change Size' button on the left hand side. (Grid size ranges from 1 to 100) 
--- main.js
+++ main.js
@@ -8,13 +8,15 @@ const sizebtn = document.getElementById('size-change');
 const clearbtn = document.getElementById('clear');
 
 let userInput = 16;
+let mouseDown = 0
 
 document.body.onload = drawGrid(userInput);
 document.body.onload = currentSize.textContent = `${userInput} x ${userInput}`;
 document.body.onload = colorSelect();
 
 
-//Changes grid size based on user input
+
+// Changes grid size based on user input
 sizebtn.addEventListener('click', () => {
     userInput = prompt('Enter a grid size from 1 to 100');
 
@@ -27,7 +29,7 @@ sizebtn.addEventListener('click', () => {
 });
 
 
-//Clears the grid of all colors
+// Clears the grid of all colors
 clearbtn.addEventListener('click', () => {
     currentSize.textContent = `${userInput} x ${userInput}`;
     drawGrid(userInput);
@@ -40,7 +42,7 @@ function randomValue(maxValue) {
 }
 
 
-//Provides a random rgb color output as a string
+// Provides a random rgb color output as a string
 function randomColor() {
     let r = randomValue(255);
     let g = randomValue(255);
@@ -50,55 +52,38 @@ function randomColor() {
 }
 
 
-// Calls colorSelect() function when a radio button is selected
+// Checks for which radio button is check and enables/ disables the 
+// the color palet based on the selection
 radioGroup.forEach((input) => {
     input.addEventListener('change', () => {    
         if (document.getElementById('select').checked){
-                enableColorPicker();
-                colorSelect();
+                colorPick.disabled = false;
             } else {
-                disableColorPicker();
-                colorSelect();
+                colorPick.disabled = true;
             }
     });  
 });
 
 
 // Checks which radio button is selected and applies the corresponding 
-// background color when hovering over the grid
-// function colorSelect() {
-function colorSelect() {
-    drawArea.addEventListener('mouseover', (event) => {
-        if (document.getElementById('black').checked) {
-            disableColorPicker();
-            event.target.style.backgroundColor = '#000000';
+// background color to draw on the grid
+function colorSelect(squareDiv) {
+        if (mouseDown === 0) {
+            return;
+        }else if (document.getElementById('black').checked) {
+            squareDiv.style.backgroundColor = '#000000';
         }else if (document.getElementById('random').checked) {
-            disableColorPicker();
-            event.target.style.backgroundColor = randomColor();
+            squareDiv.style.backgroundColor = randomColor();
         }else if (document.getElementById('select').checked) {
-            enableColorPicker();
-            event.target.style.backgroundColor = colorPick.value;
+            squareDiv.style.backgroundColor = colorPick.value;
         }else {
-            disableColorPicker();
-            event.target.style.backgroundColor = '#ffffff';
+            squareDiv.style.backgroundColor = '#ffffff';
         }
-    });
 }
 
 
-//Disable color picker
-function disableColorPicker() {
-    colorPick.disabled = true;
-}
-
-
-//Enables color picker
-function enableColorPicker() {
-    colorPick.disabled = false;
-}
-
-
-//Create divs for drawing area
+// Create divs for drawing area and allows drawing/ filling cell background 
+// based on mouse events
 function drawGrid(input) {
     userInput = input;
     deleteGrid();
@@ -109,12 +94,29 @@ function drawGrid(input) {
     for (let x = 0; x < squares; x++){
         const squareDiv = document.createElement('div');
         squareDiv.style.backgroundColor = "#ffffff";
+
+        squareDiv.addEventListener('mousedown', (e) => {
+            mouseDown = 1;
+            colorSelect(squareDiv);
+            console.log(mouseDown);
+        });
+
+        squareDiv.addEventListener('mousemove', (e) => {
+            if (mouseDown > 0){
+                colorSelect(squareDiv);
+            }
+        });
+
+        squareDiv.addEventListener('mouseup', (e) => {
+            mouseDown = 0;
+        })
+        
         drawArea.appendChild(squareDiv);
     }   
 }
 
 
-// Deletes the current grid
+// Deletes the current grid size
 function deleteGrid() {
     div = drawArea.lastElementChild;
     while(div) {
